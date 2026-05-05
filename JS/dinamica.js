d2027581813681d95658a3b4b44b24cf47d58cc0
Franklin Grisales Castrillon <franklitonadador234@gmail.com>
2026-05-04 21:45:54 -0500

    Creacion, vinculacion y puesta en funcionamiento a animacion de java script en el header

---
+++ JS/dinamica.js
@@ -0,0 +1,39 @@
+const header = document.querySelector('header');
+const logoTexto = document.querySelector('.logo h1');
+const iconoMenu = document.querySelector('.btn-menu label');
+
+const colores = {
+    'hero': {
+        fondo:  '#D3D45C',
+        texto:  '#E40605',
+        icono:  '#E40605'
+    },
+    'sinopsis': {
+        fondo:  '#8C5011',
+        texto:  '#D3D45C',
+        icono:  '#D3D45C'
+    },
+    'cortometraje': {
+        fondo:  '#E40605',
+        texto:  '#F9C521',
+        icono:  '#F9C521'
+    }
+};
+
+const secciones = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', () => {
+    let seccionActual = 'hero';
+
+    secciones.forEach(seccion => {
+        const top = seccion.offsetTop - 80;
+        if (window.scrollY >= top) {
+            seccionActual = seccion.id;
+        }
+    });
+
+    const config = colores[seccionActual];
+    header.style.background = config.fondo;
+    logoTexto.style.color   = config.texto;
+    iconoMenu.style.color   = config.icono;
+});
